334b95f43734e3cf86ad56b972912d3d71fbb4ae
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-05-22 19:41:42 +0530

    sets up skeleton for project

--- src/main/java/io/supertokens/javalin/SuperTokens.java
+++ src/main/java/io/supertokens/javalin/SuperTokens.java
@@ -1,5 +1,39 @@
 package io.supertokens.javalin;
 
+
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
 public class SuperTokens {
+
+    public Session createNewSession(@NotNull Context ctx, @NotNull  String userId,
+                                 @Nullable  Map jwtPayload, @Nullable Map sessionData) {
+        // TODO:
+        return null;
+    }
+
+    public Session getSession(@NotNull Context ctx, boolean doAntiCSRFCheck) {
+        // TODO:
+        return null;
+    }
+
+    public Session refreshSession(@NotNull Context ctx) {
+        // TODO:
+        return null;
+    }
+
+    public int revokeAllSessionsForUser(@NotNull String userId) {
+        // TODO:
+        return 0;
+    }
+
+    public String[] getAllSessionHandlesForUser(@NotNull String userId) {
+        // TODO:
+        return new String[]{};
+    }
+
 
 }
